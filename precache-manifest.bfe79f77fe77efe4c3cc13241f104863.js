4323ef8194a49b8d77b030d0fea6c1eaafe64831
Peter Jochem <peterpennrichjochem@gmail.com>
2020-04-11 19:49:06 -0500

    Updates

---
+++ precache-manifest.bfe79f77fe77efe4c3cc13241f104863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb3d64d9b68fce3da3849f7cadc2aa7",
+    "url": "/React_TicTac/index.html"
+  },
+  {
+    "revision": "be4c366dc084805325f5",
+    "url": "/React_TicTac/static/css/main.45227cd1.chunk.css"
+  },
+  {
+    "revision": "875f7dcd1386acc6cd92",
+    "url": "/React_TicTac/static/js/2.062b21eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_TicTac/static/js/2.062b21eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4c366dc084805325f5",
+    "url": "/React_TicTac/static/js/main.6bbfc2f3.chunk.js"
+  },
+  {
+    "revision": "2973b9b8af4413cb945a",
+    "url": "/React_TicTac/static/js/runtime-main.e977b2d2.js"
+  }
+]);

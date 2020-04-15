727209e0601b422eb3a467beb81f71a88b529eb2
Peter Jochem <peterpennrichjochem@gmail.com>
2020-04-14 21:51:36 -0500

    Updates

---
+++ precache-manifest.74fd951143fa914d1ee13fe0f0fe0544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0568d138b5e79ea8d169d26d797b12",
+    "url": "/React_Tutorial/index.html"
+  },
+  {
+    "revision": "043f61e98eda84d9cacd",
+    "url": "/React_Tutorial/static/css/main.76509e48.chunk.css"
+  },
+  {
+    "revision": "455cb172afce9fad6f27",
+    "url": "/React_Tutorial/static/js/2.c3c5071f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Tutorial/static/js/2.c3c5071f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043f61e98eda84d9cacd",
+    "url": "/React_Tutorial/static/js/main.604bfbe2.chunk.js"
+  },
+  {
+    "revision": "7c3e962aff7e64cfa00d",
+    "url": "/React_Tutorial/static/js/runtime-main.4dc5790e.js"
+  },
+  {
+    "revision": "554fff83f1bccea06876289741d09da2",
+    "url": "/React_Tutorial/static/media/links.554fff83.yaml"
+  }
+]);

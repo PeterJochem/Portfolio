88f9a4b956cf5f26ce2ea5d456f5cad924253467
PeterJochem <peterjochem2020@u.northwestern.edu>
2020-11-30 14:11:00 -0600

    Updates

--- precache-manifest.637587c64cd2b16e1db2f545fc37d92d.js
+++ precache-manifest.637587c64cd2b16e1db2f545fc37d92d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb94ae8dee3e8761dc60c608b6c357b0",
+    "revision": "444c48f7b95d39e7dbff4e4f7c01a7c0",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "17f63196100c13a14399",
-    "url": "/Portfolio/static/css/main.da449b6a.chunk.css"
+    "revision": "e73b53607848f9ccc9b7",
+    "url": "/Portfolio/static/css/main.34bcf23c.chunk.css"
   },
   {
     "revision": "bc2e821f20212bd0e7f9",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio/static/js/2.c83b668a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "17f63196100c13a14399",
+    "revision": "e73b53607848f9ccc9b7",
     "url": "/Portfolio/static/js/main.fc71512d.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.7a713e192638c21a0a7d8005ec6c0437.js"
+  "/Portfolio/precache-manifest.637587c64cd2b16e1db2f545fc37d92d.js"
 );
 
 self.addEventListener('message', (event) => {
